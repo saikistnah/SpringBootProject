867ef2bf7f5986eda555cfc828172bd406e360b4
saikrishna <saikistnah@gmail.com>
2017-11-26 21:50:20 +0800

    added testcases

--- src/test/java/com/sai/user/controllerTest/UserRegistrationControllerTests.java
+++ src/test/java/com/sai/user/controllerTest/UserRegistrationControllerTests.java
@@ -1,21 +1,28 @@
 package com.sai.user.controllerTest;
 
 
-import static org.hamcrest.Matchers.is;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.TestContext;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -64,11 +71,27 @@ public class UserRegistrationControllerTests {
 		user.setActive(false);
 		user.setId(0);
 		
+		User mockuser = new User();
+		user.setfName("sai");
+		user.setlName("keish");
+		user.setEmail("sk1@gmail.com");
+		user.setBirthDate("26-may-1999");
+		user.setPinCode("12345");
+		user.setActive(false);
+		user.setId(1);
+		
+		List<User> li = new ArrayList<>();
+		li.add(mockuser);
+		
+		Mockito.when(userUtils.validateDate("26-may-1999")).thenReturn(true);
+		Mockito.when(userService.getUsers()).thenReturn(li);
+		
 		this.mockMvc.perform(post("/create")
                 .contentType(TestUtil.APPLICATION_JSON_UTF8)
                 .content(TestUtil.convertObjectToJsonBytes(user))
         )
-                .andExpect(status().isOk());
+                .andExpect(status().isOk()).
+                andExpect(content().contentType(TestUtil.APPLICATION_JSON_UTF8));
  
 	}
 	
@@ -82,6 +105,19 @@ public class UserRegistrationControllerTests {
 		user.setPinCode("12345");
 		user.setActive(false);
 		user.setId(0);
+		List<User> li = new ArrayList<>();
+		li.add(user);
+		
+		Map<String,String> mp = new HashMap<>();
+		mp.put("resMsg", "Email already exists");
+		mp.put("userId", Integer.toString(1));
+		mp.put("valErrors", "Email already exists");
+		
+		
+		Mockito.when(userUtils.validateDate("26-may-1999")).thenReturn(true);
+		Mockito.when(userService.getUsers()).thenReturn(li);
+		Mockito.when(customExceptionWithHttpStatusCode.errorHandle("Email already exists", 1, "Email already exists")).thenReturn( new ResponseEntity<Object>(mp,HttpStatus.CONFLICT));
+		
 		
 		this.mockMvc.perform(post("/create")
                 .contentType(TestUtil.APPLICATION_JSON_UTF8)
@@ -102,6 +138,21 @@ public class UserRegistrationControllerTests {
 		user.setActive(false);
 		user.setId(0);
 		
+		User mockuser = new User();
+		user.setfName("sai");
+		user.setlName("keish");
+		user.setEmail("sk1@gmail.com");
+		user.setBirthDate("26-may-1999");
+		user.setPinCode("12345");
+		user.setActive(false);
+		user.setId(1);
+		
+		List<User> li = new ArrayList<>();
+		li.add(mockuser);
+		
+		Mockito.when(userUtils.validateDate("26-may-1999")).thenReturn(true);
+		Mockito.when(userService.getUsers()).thenReturn(li);
+		
 		this.mockMvc.perform(put("/update/1")
                 .contentType(TestUtil.APPLICATION_JSON_UTF8)
                 .content(TestUtil.convertObjectToJsonBytes(user))
@@ -120,6 +171,28 @@ public class UserRegistrationControllerTests {
 		user.setPinCode("12345");
 		user.setActive(false);
 		user.setId(0);
+		
+		User mockuser = new User();
+		user.setfName("sai");
+		user.setlName("keish");
+		user.setEmail("sk1@gmail.com");
+		user.setBirthDate("26-may-1999");
+		user.setPinCode("12345");
+		user.setActive(false);
+		user.setId(1);
+		
+		List<User> li = new ArrayList<>();
+		li.add(mockuser);
+		
+		Map<String,String> mp = new HashMap<>();
+		mp.put("resMsg", "Please Enter valid Date");
+		mp.put("userId", Integer.toString(1));
+		mp.put("valErrors", "Please Enter valid Date");
+		
+		
+		Mockito.when(userUtils.validateDate("26-may-1999")).thenReturn(false);
+		Mockito.when(userService.getUsers()).thenReturn(li);
+		Mockito.when(customExceptionWithHttpStatusCode.errorHandle("Please Enter valid Date", 1, "Please Enter valid Date")).thenReturn( new ResponseEntity<Object>(mp,HttpStatus.CONFLICT));
 		
 		mockMvc.perform(put("/update/1")
                 .contentType(TestUtil.APPLICATION_JSON_UTF8)
